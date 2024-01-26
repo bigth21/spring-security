ff91af3d4b54ea128685fd39e2379e565092cbae
bigth21 <taekhyeon.nam@gmail.com>
2024-01-26 17:14:53 +0900

    Create HomeController and configure SecurityFilterChain

---
+++ src/test/java/com/example/springsecurity/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
+class HomeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Home"));
+    }
+}
