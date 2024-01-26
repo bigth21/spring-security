7c76238b7bbb587be8ab023a1613d8cedb83c382
bigth21 <taekhyeon.nam@gmail.com>
2024-01-26 19:20:04 +0900

    Polishing

--- src/test/java/com/example/springsecurity/web/HelloControllerTest.java
+++ src/test/java/com/example/springsecurity/web/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.springsecurity;
+package com.example.springsecurity.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/example/springsecurity/web/HomeControllerTest.java
+++ src/test/java/com/example/springsecurity/web/HomeControllerTest.java
@@ -1,5 +1,6 @@
-package com.example.springsecurity;
+package com.example.springsecurity.web;
 
+import com.example.springsecurity.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
