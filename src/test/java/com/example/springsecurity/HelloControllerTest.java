c342a21dd381490d2b6f1dbc88a890444d83f67a
bigth21 <taekhyeon.nam@gmail.com>
2024-01-26 16:49:01 +0900

    Create HelloController and hello method

---
+++ src/test/java/com/example/springsecurity/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class HelloControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    SecurityProperties securityProperties;
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(get("/hello")
+                        .with(httpBasic(securityProperties.getUser().getName(), securityProperties.getUser().getPassword())))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello"));
+    }
+}
