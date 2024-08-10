c511a6c0e9f9438846351d1327d588b64df59776
bigth21 <taekhyeon.nam@gmail.com>
2024-08-10 23:30:20 +0900

    Init

---
+++ src/test/java/apes/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package apes.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
