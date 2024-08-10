6f63b33deb517b29d8fc009d20be0c6368cb2e03
bigth21 <taekhyeon.nam@gmail.com>
2024-08-11 00:01:33 +0900

    Polishing

--- src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
+++ src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
@@ -3,23 +3,26 @@ package apes.springsecurity.infrastructure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(c -> c
-                        .requestMatchers(HttpMethod.GET, "/").authenticated()
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-                .formLogin(c -> c
-                        .defaultSuccessUrl("/"));
-
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
+
 }
