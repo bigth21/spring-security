4e4a2e8591271eb6286a531fe2a63cbfd89dca76
bigth21 <taekhyeon.nam@gmail.com>
2024-08-11 00:01:50 +0900

    Polishing

--- src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
+++ src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
@@ -2,11 +2,9 @@ package apes.springsecurity.infrastructure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
