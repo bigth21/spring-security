0e7d66e6d164b1bde1b6f3d9499c88f1260472e6
bigth21 <taekhyeon.nam@gmail.com>
2024-08-15 15:24:49 +0900

    Create InMemoryUserDetailsService bean

--- src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
+++ src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
@@ -23,6 +27,16 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .addFilterBefore(new TenantFilter(), AuthorizationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
 }
