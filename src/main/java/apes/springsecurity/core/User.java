a3d3aa53cc498427be5adaf348a43d9b89e226c1
bigth21 <taekhyeon.nam@gmail.com>
2024-08-21 00:04:48 +0900

    Polishing

--- src/main/java/apes/springsecurity/core/User.java
+++ src/main/java/apes/springsecurity/core/User.java
@@ -5,13 +5,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(indexes = @Index(name = "ix_username", columnList = "username", unique = true))
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String username;
     @Column(nullable = false)
     private String password;
