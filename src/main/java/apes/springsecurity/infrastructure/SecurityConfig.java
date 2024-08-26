9d230926ebb8187900ae653ab14eef6cf2067523
bigth21 <taekhyeon.nam@gmail.com>
2024-08-27 00:25:49 +0900

    Polishing

--- src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
+++ src/main/java/apes/springsecurity/infrastructure/SecurityConfig.java
@@ -4,8 +4,6 @@ import apes.springsecurity.core.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
