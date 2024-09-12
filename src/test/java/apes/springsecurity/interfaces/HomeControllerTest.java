e0b8f36cff9c154f7b4155ae86b759e9ae7f025f
bigth21 <taekhyeon.nam@gmail.com>
2024-09-12 18:27:49 +0900

    Update @MockMvcUser-related text code

---
+++ src/test/java/apes/springsecurity/interfaces/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package apes.springsecurity.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @WithAnonymousUser
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = "STAFF")
+    @Test
+    void staff() throws Exception {
+        mockMvc.perform(get("/staff"))
+                .andExpect(status().isOk());
+    }
+}
